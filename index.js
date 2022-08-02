9032153f539d5fd68a9f11d81cbbf54d24f4f6d3
RosvaldasG <rosvaldasggg@gmail.com>
2022-08-02 20:47:23 +0300

    delete function

--- index.js
+++ index.js
@@ -3,23 +3,35 @@ let arr = [
   { id: 102, task: "buy eggs" },
   { id: 103, task: "buy bear" },
   { id: 104, task: "buy cockut" },
-  { id: 104, task: "buy tomatoes" },
-  { id: 104, task: "buy cucmber" },
 ];
 
 document.getElementById("addBtn").addEventListener("click", () => {
   const inputValue = document.getElementById("taskInput").value;
-  if (inputValue == "") {
+  if (!inputValue.trim()) {
     alert("Neteisinga reikšmė");
   } else {
     const newTask = {
-      id: new Date().getTime,
+      id: new Date().getTime(),
       task: inputValue,
     };
     arr.push(newTask);
     drawTaskList();
   }
 });
+
+function deleteItem() {
+  // console.log(
+  //   this.outerHTML
+  //     .split(" ")
+  //     .filter((item) => item.includes("taskid"))[0]
+  //     .split("=")[1]
+  // );
+  console.log(arr);
+  console.log(this.id);
+  arr.shift(this.id);
+  drawTaskList();
+  console.log(arr);
+}
 
 const drawTaskList = () => {
   const taskList = document.getElementById("taskList");
@@ -29,18 +41,27 @@ const drawTaskList = () => {
     const myLi = document.createElement("li");
     const myInput = document.createElement("Input");
     const myLabel = document.createElement("Label");
+    const delBut = document.createElement("Button");
     //   ---Priskiria clases
     myLi.className = "list-group-item";
-    myInput.className = "form-check-input me-1";
-    myLabel.className = "form-check-label";
+    myInput.className = "form-check-input me-1 col-1";
+    myLabel.className = "form-check-label col-6";
+    delBut.className = "btn btn-success btn-sm col-5";
     //   ----Priskiria atributus, kaip TYPE, ID ir FOR
     myInput.setAttribute("type", "checkbox");
     myInput.setAttribute("id", value.id);
     myLabel.setAttribute("for", value.id);
+    // delBut.setAttribute("taskId", value.id);
+    delBut.setAttribute("id", value.id);
+    delBut.setAttribute("type", "button");
+
+    delBut.onclick = deleteItem;
+
     //-------------
     myLabel.textContent = value.task;
+    delBut.textContent = "Delete";
     // --- išveda į ekraną
-    myLi.append(myInput, myLabel);
+    myLi.append(myInput, myLabel, delBut);
     taskList.append(myLi);
   });
 };
